6646ed9bb41b3d1975a5808e6cbf98e3e99b0499
yoshiken <yskn.beer@gmail.com>
2018-04-11 11:33:45 +0900

    会員ページ(ログイン成功後遷移画面)作成

---
+++ app/action/Photographer/Home.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ *  Photographer/Home.php
+ *
+ *  @author     {$author}
+ *  @package    Yeahcheese
+ */
+
+/**
+ *  photographer_home Form implementation.
+ *
+ *  @author     {$author}
+ *  @access     public
+ *  @package    Yeahcheese
+ */
+class Yeahcheese_Form_PhotographerHome extends Yeahcheese_ActionForm
+{
+    /**
+     *  @access protected
+     *  @var    array   form definition.
+     */
+    public $form = array(
+       /*
+        *  TODO: Write form definition which this action uses.
+        *  @see http://ethna.jp/ethna-document-dev_guide-form.html
+        *
+        *  Example(You can omit all elements except for "type" one) :
+        *
+        *  'sample' => array(
+        *      // Form definition
+        *      'type'        => VAR_TYPE_INT,    // Input type
+        *      'form_type'   => FORM_TYPE_TEXT,  // Form type
+        *      'name'        => 'Sample',        // Display name
+        *
+        *      //  Validator (executes Validator by written order.)
+        *      'required'    => true,            // Required Option(true/false)
+        *      'min'         => null,            // Minimum value
+        *      'max'         => null,            // Maximum value
+        *      'regexp'      => null,            // String by Regexp
+        *
+        *      //  Filter
+        *      'filter'      => 'sample',        // Optional Input filter to convert input
+        *      'custom'      => null,            // Optional method name which
+        *                                        // is defined in this(parent) class.
+        *  ),
+        */
+    );
+
+    /**
+     *  Form input value convert filter : sample
+     *
+     *  @access protected
+     *  @param  mixed   $value  Form Input Value
+     *  @return mixed           Converted result.
+     */
+    /*
+    protected function _filter_sample($value)
+    {
+        //  convert to upper case.
+        return strtoupper($value);
+    }
+    */
+}
+
+/**
+ *  photographer_home action implementation.
+ *
+ *  @author     {$author}
+ *  @access     public
+ *  @package    Yeahcheese
+ */
+class Yeahcheese_Action_PhotographerHome extends Yeahcheese_ActionClass
+{
+    /**
+     *  preprocess of photographer_home Action.
+     *
+     *  @access public
+     *  @return string    forward name(null: success.
+     *                                false: in case you want to exit.)
+     */
+    public function prepare()
+    {
+        /**
+        if ($this->af->validate() > 0) {
+            // forward to error view (this is sample)
+            return 'error';
+        }
+        $sample = $this->af->get('sample');
+        */
+        return null;
+    }
+
+    /**
+     *  photographer_home action implementation.
+     *
+     *  @access public
+     *  @return string  forward name.
+     */
+    public function perform()
+    {
+        return 'photographer_home';
+    }
+}
