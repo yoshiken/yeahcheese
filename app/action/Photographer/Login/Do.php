cbfb7b31c6165ad7ca462105a5100565203361d5
yoshiken <yskn.beer@gmail.com>
2018-04-11 16:56:27 +0900

    メールアドレス・パスワードのバリデーション機能実装

--- app/action/Photographer/Login/Do.php
+++ app/action/Photographer/Login/Do.php
@@ -3,16 +3,28 @@
 class Yeahcheese_Form_PhotographerLoginDo extends Yeahcheese_ActionForm
 {
     public $form = array(
+    'mailaddress' => [
+      'name'          => 'メールアドレス',
+      'required'      => true,
+      'custom' => 'checkMailaddress',
+      'type'          => VAR_TYPE_STRING,
+      ],
+    'password' => [
+      'name'          => 'パスワード',
+      'required'      => true,
+      'type'          => VAR_TYPE_STRING,
+      ],
     );
 }
 class Yeahcheese_Action_PhotographerLoginDo extends Yeahcheese_ActionClass
 {
     public function prepare()
     {
-        return null;
+        if ($this->af->validate() > 0) {
+            return 'photographer_home';
+        }
     }
     public function perform()
     {
-        return 'photographer_home';
     }
 }
