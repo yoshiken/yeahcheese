d6f1a0701a5fb0ffcb80ddcc15405a48bcea9f21
yoshiken <yskn.beer@gmail.com>
2018-04-11 10:04:58 +0900

    不要な行を削除

--- app/view/Creat/Photographer.php
+++ app/view/Creat/Photographer.php
@@ -1,25 +1,6 @@
 <?php
-/**
- *  Creat/Photographer.php
- *
- *  @author     {$author}
- *  @package    Yeahcheese
- */
-
-/**
- *  creat_photographer view implementation.
- *
- *  @author     {$author}
- *  @access     public
- *  @package    Yeahcheese
- */
 class Yeahcheese_View_CreatPhotographer extends Yeahcheese_ViewClass
 {
-    /**
-     *  preprocess before forwarding.
-     *
-     *  @access public
-     */
     public function preforward()
     {
     }
