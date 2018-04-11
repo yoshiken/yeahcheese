8eb195368a5505e2fa42405ab6e15edcbae65543
yoshiken <yskn.beer@gmail.com>
2018-04-11 12:39:29 +0900

    フォームのクラス記述間違えを修正

--- app/action/Creat/Photographer/Do.php
+++ app/action/Creat/Photographer/Do.php
@@ -1,20 +1,22 @@
 <?php
-  class Yeahcheese_Action_CreatPhotographerDo extends Yeahcheese_ActionClass
+  class Yeahcheese_Form_CreatPhotographerDo extends Yeahcheese_ActionForm
   {
       public $form = array(
-    'mailaddress' => [
-      'name'          => 'メールアドレス',
-      'required'      => true,
-      'custom' => 'checkMailaddress',
-      'type'          => VAR_TYPE_STRING,
-      ],
-    'password' => [
-      'name'          => 'パスワード',
-      'required'      => true,
-      'type'          => VAR_TYPE_STRING,
-      ],
-  );
-
+        'mailaddress' => [
+          'name'          => 'メールアドレス',
+          'required'      => true,
+          'custom' => 'checkMailaddress',
+          'type'          => VAR_TYPE_STRING,
+          ],
+        'password' => [
+          'name'          => 'パスワード',
+          'required'      => true,
+          'type'          => VAR_TYPE_STRING,
+          ],
+      );
+  }
+  class Yeahcheese_Action_CreatPhotographerDo extends Yeahcheese_ActionClass
+  {
       public function perform()
       {
           if ($this->af->validate() > 0) {
