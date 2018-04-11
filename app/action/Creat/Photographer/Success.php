e4f142c98d0330d917d4ea982bb5130845be7de4
yoshiken <yskn.beer@gmail.com>
2018-04-11 10:09:45 +0900

    登録成功画面作成

---
+++ app/action/Creat/Photographer/Success.php
@@ -0,0 +1,13 @@
+<?php
+class Yeahcheese_Action_CreatPhotographerSuccess extends Yeahcheese_ActionClass
+{
+    public function prepare()
+    {
+        return null;
+    }
+
+    public function perform()
+    {
+        return 'creat_photographer_success';
+    }
+}
