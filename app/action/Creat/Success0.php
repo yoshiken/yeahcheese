36de4ed333e29996d69a2a34526d6aff7a14b0bf
yoshiken <yskn.beer@gmail.com>
2018-04-11 18:44:16 +0900

    記憶に無いアクションファイルがあるので削除

--- app/action/Creat/Success0.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Yeahcheese_Action_CreatPhotographerSuccess extends Yeahcheese_ActionClass
-{
-    public function prepare()
-    {
-        return 'creat_photographer_success';
-    }
-
-}
