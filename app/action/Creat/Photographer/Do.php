48bf8e7a3f341985d6d238b6061dafc77f3904e0
yoshiken <yskn.beer@gmail.com>
2018-04-12 11:19:48 +0900

    そもそもActionFormに書いて無かったので機能していないバグを修正

--- app/action/Creat/Photographer/Do.php
+++ app/action/Creat/Photographer/Do.php
@@ -1,23 +1,31 @@
 <?php
-class Yeahcheese_Action_CreatPhotographerDo extends Yeahcheese_ActionClass
+class Yeahcheese_Form_CreatPhotographerDo extends Yeahcheese_ActionForm
 {
     public $form = array(
-    'mailaddress' => [
-      'name'          => 'メールアドレス',
-      'required'      => true,
-      'custom'        => 'checkMailaddress',
-      'type'          => VAR_TYPE_STRING,
-      ],
-    'password' => [
-      'name'          => 'パスワード',
-      'required'      => true,
-      'type'          => VAR_TYPE_STRING,
-      ],
+      'mailaddress' => [
+        'name'          => 'メールアドレス',
+        'required'      => true,
+        'custom'        => 'checkMailaddress',
+        'type'          => VAR_TYPE_STRING,
+        ],
+        'password' => [
+          'name'          => 'パスワード',
+          'required'      => true,
+          'type'          => VAR_TYPE_STRING,
+        ],
+        'password_confirm' => [
+          'name'          => 'パスワード確認用',
+          'required'      => true,
+          'type'          => VAR_TYPE_STRING,
+        ],
     );
+}
 
+class Yeahcheese_Action_CreatPhotographerDo extends Yeahcheese_ActionClass
+{
     public function perform()
     {
-        if ($this->af->validate() > 0 && $this->af->get('password') === $this->af->get('password_confirm')) {
+        if ($this->af->validate() > 0 || $this->af->get('password') !== $this->af->get('password_confirm')) {
               return 'creat_photographer';
         } else {
               return 'creat_photographer_success';
