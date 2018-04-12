05580580d3c766f350a2e81de003af9b64f01659
yoshiken <yskn.beer@gmail.com>
2018-04-12 09:19:32 +0900

    パスワードの入力確認をjsからformのrequiredに移行

--- www/js/creat/form.js
+++ www/js/creat/form.js
@@ -1,10 +1,8 @@
-$('#form_creat_photographer').submit(function() {
-  
-  if ($("#password")[0].value == '' || $("#password_confirm")[0].value == '') {
-    alert('入力がありません');
-    return false;
-  }else if ($("#password")[0].value != $("#password_confirm")[0].value) {
-    alert('パスワードが違っています');
-    return false;
-  }
+$(function() {
+  $('#form_creat_photographer').submit(function() {
+    if ($("#password")[0].value != $("#password_confirm")[0].value) {
+      alert('パスワードが違っています');
+      return false;
+    }
+  })
 });
