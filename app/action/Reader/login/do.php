51d007c17f5f2d68f40532d9560c7910cfe136a2
yoshiken <yskn.beer@gmail.com>
2018-04-18 14:25:14 +0900

    認証キーの確認アクションクラス生成

---
+++ app/action/Reader/login/do.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ *  Reader/Login/Do.php
+ *  Action 認証キーの認証
+ */
+class Yeahcheese_Form_PhotographerLoginDo extends Yeahcheese_ActionForm
+{
+
+    public $form = [
+    'mailaddress' => [
+      'name'          => '認証キー',
+      'required'      => true,
+      'type'          => VAR_TYPE_STRING,
+      ],
+    ];
+}
+
+class Yeahcheese_Action_ReaderLoginDo extends Yeahcheese_ActionClass
+{
+    public function prepare()
+    {
+        if ($this->af->validate() > 0) {
+            return 'reader_home';
+        }
+        return null;
+    }
+
+    public function perform()
+    {
+        return 'event_info';
+    }
+}
