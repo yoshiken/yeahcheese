8e0d5ebcba9e2e7d339cad3023fb0ee8f0df4214
yoshiken <yskn.beer@gmail.com>
2018-04-18 14:52:16 +0900

    認証キーの認証処理を実装

--- app/action/Reader/login/do.php
+++ app/action/Reader/login/do.php
@@ -3,7 +3,7 @@
  *  Reader/Login/Do.php
  *  Action 認証キーの認証
  */
-class Yeahcheese_Form_PhotographerLoginDo extends Yeahcheese_ActionForm
+class Yeahcheese_Form_ReaderLoginDo extends Yeahcheese_ActionForm
 {
 
     public $form = [
@@ -20,6 +20,12 @@ class Yeahcheese_Action_ReaderLoginDo extends Yeahcheese_ActionClass
     public function prepare()
     {
         if ($this->af->validate() > 0) {
+            return 'reader_home';
+        }
+        $rl = $this->backend->getManager('event');
+        $eventdate = $rl->loadEventPutKey($this->af->get('event_key'));
+        if (Ethna::isError($eventdate)) {
+            $this->ae->addObject('event_key_error', $eventdate);
             return 'reader_home';
         }
         return null;
