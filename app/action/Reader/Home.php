5cc597b210ddf5a54d3acad3e8d4b7ff253cc9af
yoshiken <yskn.beer@gmail.com>
2018-04-18 14:20:55 +0900

    閲覧者ホームページ作成

---
+++ app/action/Reader/Home.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *  Reader/Home.php
+ *  Action 閲覧者ホーム画面
+ */
+class Yeahcheese_Action_ReaderHome extends Yeahcheese_ActionClass
+{
+    public function perform()
+    {
+        return 'reader_home';
+    }
+}
