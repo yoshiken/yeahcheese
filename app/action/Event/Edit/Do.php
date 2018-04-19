bdaff5abee418e2c778d47b2666a9996e6c6fd70
yoshiken <yskn.beer@gmail.com>
2018-04-19 11:41:45 +0900

    イベント更新機能を追加

--- app/action/Event/Edit/Do.php
+++ app/action/Event/Edit/Do.php
@@ -31,9 +31,12 @@ class Yeahcheese_Form_EventEditDo extends Yeahcheese_ActionForm
         'required'      => true,
         'type'          => VAR_TYPE_DATETIME,
         ],
+    'event_photo_tmp' => [
+        'name'          => 'イベント写真',
+        'type'          => array(VAR_TYPE_STRING),
+        ],
     'event_photo_url' => [
         'name'          => 'イベント写真',
-        'required'      => true,
         'type'          => array(VAR_TYPE_STRING),
         ],
     );
@@ -43,15 +46,29 @@ class Yeahcheese_Action_EventEditDo extends Yeahcheese_ActionClass
 {
     public function prepare()
     {
+        $record['event_id'] = $this->af->get('event_id');
+        $record['event_name'] = $this->af->get('event_name');
+        $record['event_key'] = $this->af->get('event_key');
+        $record['event_start_day'] = $this->af->get('event_start_day');
+        $record['event_end_day'] = $this->af->get('event_end_day');
+        $record['photographer_id'] = $this->session->get('userid')[id];
+
+        $ev = $this->backend->getManager('event');
+        $updateevent = $ev->updateEvent($record);
+        if (Ethna::isError($updateevent)) {
+            $this->ae->addObject('dberror', $updateevent);
+            return 'event_edit';
+        }
+
         return null;
     }
 
     public function perform()
     {
         if ($this->af->validate() > 0) {
-            return 'event_create';
+            return 'event_edit';
         } else {
-            return 'event_info';
+            return 'event_edit_success';
         }
     }
 }
