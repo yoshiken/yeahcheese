d8890663041875d1f205f18fde082eb0a54ba97a
yoshiken <yskn.beer@gmail.com>
2018-04-19 17:18:37 +0900

    不要なバリデーションを削除

--- app/action/Event/Create.php
+++ app/action/Event/Create.php
@@ -3,33 +3,6 @@
  *  Event/Create.php
  *  Action イベント画面表示
  */
-class Yeahcheese_Form_EventCreate extends Yeahcheese_ActionForm
-{
-    public $form = array(
-     'event_name' => [
-       'name'          => 'イベント名',
-       'required'      => true,
-       'type'          => VAR_TYPE_STRING,
-       ],
-     'event_start_day' => [
-       'name'          => '公開開始日',
-       'required'      => true,
-       'type'          => VAR_TYPE_DATETIME,
-       ],
-     'event_end_day' => [
-       'name'          => '公開終了日',
-       'required'      => true,
-       'type'          => VAR_TYPE_DATETIME,
-       ],
-     'event_photo' => [
-       'name'          => '写真',
-       'required'      => true,
-       'type'          => VAR_TYPE_FILE,
-       'file_size_max' => '5000KB',
-       ],
-    );
-}
-
 class Yeahcheese_Action_EventCreate extends Yeahcheese_ActionClass
 {
     public function perform()
