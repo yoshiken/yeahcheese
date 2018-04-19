a85fdfce34642e107a20db2db50621256d1cada4
yoshiken <yskn.beer@gmail.com>
2018-04-19 18:45:21 +0900

    コンフリ解消

--- app/Yeahcheese_EventManager.php
+++ app/Yeahcheese_EventManager.php
@@ -40,10 +40,10 @@ class Yeahcheese_EventManager extends Ethna_AppManager
     /* 写真家(ユーザー)がイベントを担当しているか
      * 担当しているならイベントIDを返す
      *
-     * @param string $photographer_id
-     * @return mixed 正常時:array、異常時：Ethna_Error
+     * @param integer $photographer_id
+     * @return mixed 正常時:null、異常時：Ethna_Error
      */
-    public function hasEvents(string $photographer_id)
+    public function hasEvents(int $photographer_id)
     {
         $sql = "
             SELECT *
