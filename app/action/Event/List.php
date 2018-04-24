1366cea00d9481bc4b3a225d9ba63f303b2d86a2
yoshiken <yskn.beer@gmail.com>
2018-04-24 10:40:05 +0900

    文字列宣言がおかしかったので修正 17#pullrequestreview-114321166

--- app/action/Event/List.php
+++ app/action/Event/List.php
@@ -9,9 +9,15 @@ class Yeahcheese_Action_EventList extends Yeahcheese_ActionClass
     {
         $ev = $this->backend->getManager('event');
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 
 >>>>>>> Sessionでユーザー状態を判別できるようにした
+=======
+
+=======
+>>>>>>> 文字列宣言がおかしかったので修正 17#pullrequestreview-114321166
+>>>>>>> 文字列宣言がおかしかったので修正 17#pullrequestreview-114321166
         $hasevent = $ev->hasEvents($this->session->get('userid')['id']);
         if (Ethna::isError($hasevent)) {
             $this->ae->addObject('hasnotevent', $hasevent);
