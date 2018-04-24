94420de7cde160d77d9b8c7f39e48be8ed2937db
yoshiken <yskn.beer@gmail.com>
2018-04-24 10:37:42 +0900

    インデント修正 17#pullrequestreview-114318775 17#pullrequestreview-114319138 17#pullrequestreview-114323993

--- app/action/Event/Edit/Do.php
+++ app/action/Event/Edit/Do.php
@@ -63,7 +63,11 @@ class Yeahcheese_Form_EventEditDo extends Yeahcheese_ActionForm
         'required'      => true,
         'type'          => VAR_TYPE_DATETIME,
         ],
+<<<<<<< HEAD
     'event_photo'       => [
+=======
+    'event_photo' => [
+>>>>>>> インデント修正
         'type'          => [VAR_TYPE_FILE],
         'name'          => '写真',
         'required'      => true,
